62ed2f0f8ad28b1c7b4d33e156906339b72eb9c4
Olena-Vozniuk <110108199+Olena-Vozniuk@users.noreply.github.com>
2023-04-24 22:51:33 +0200

    Додано файли для gh-pages гілки

--- src/components/Logo/Logo.jsx
+++
@@ -1,9 +0,0 @@
-
-import { Link } from 'react-router-dom';
-import LogoImage from 'images/omgTweet.png'
-
-export const Logo = () => (
-  <Link to={'/'}>
-    <img src={LogoImage} alt="website's logo" width="130px" style={{ paddingLeft: '16px'}} />
-  </Link>
-);
